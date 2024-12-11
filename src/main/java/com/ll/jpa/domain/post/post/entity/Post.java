f9061d8792e9a595df429c9cc0421427aba5f35e
solji <solji.choi89@gmail.com>
2024-12-11 12:22:11 +0900

    post entity class

---
+++ src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
@@ -0,0 +1,26 @@
+package com.ll.jpa.domain.post.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Post {
+    //long -> null 안됨, Long -> null 가능
+    //JPA 엔티티 클래스 특성상 id 필드는 null이 가능하도록
+    @Id //primary key
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    @Column(length = 100)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private boolean blind;
+}

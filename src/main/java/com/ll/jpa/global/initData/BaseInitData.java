e921881d7f2bd4af83b7c9ff8bddb7cf075c12cd
solji <solji.choi89@gmail.com>
2024-12-12 10:48:21 +0900

    Transactional

--- src/main/java/com/ll/jpa/global/initData/BaseInitData.java
+++ src/main/java/com/ll/jpa/global/initData/BaseInitData.java
@@ -2,13 +2,11 @@ package com.ll.jpa.global.initData;
 
 import com.ll.jpa.domain.post.post.entity.Post;
 import com.ll.jpa.domain.post.post.service.PostService;
-import com.ll.jpa.standard.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
@@ -17,39 +15,17 @@ public class BaseInitData {
     private final PostService postService;
 
     @Bean
-    @Order(1)
     public ApplicationRunner baseInitData1ApplicationRunner() {
-        return args -> {
-            if(postService.count() > 0) return;
-
-            Post post1 = postService.write("title1", "content1");
-            Post post2 = postService.write("title2", "content2");
-            Post post3 = postService.write("title3", "content3");
-        };
-    }
-
-    @Bean
-    @Order(2)
-    public ApplicationRunner baseInitData2ApplicationRunner() {
         return new ApplicationRunner() {
             @Transactional
             @Override
             public void run(ApplicationArguments args) throws Exception {
-                Ut.thread.sleep(1000);
+                if (postService.count() > 0) return;
 
-                Post post1 = postService.findById(1).get();
-                postService.modify(post1, "title1-1", "content1-1");
+                Post post1 = postService.write("title1", "content1");
+                Post post2 = postService.write("title2", "content2");
+                Post post3 = postService.write("title3", "content3");
             }
-        };
-    }
-
-    @Bean
-    @Order(3)
-    public ApplicationRunner baseInitData3ApplicationRunner() {
-        return args -> {
-            Post post4 = postService.write("title4", "content4");
-
-            postService.delete(post4);
         };
     }
 }

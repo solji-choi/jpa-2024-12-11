d1cdd064523bc215d1cd8c24fa4725e56d2dcd79
solji <solji.choi89@gmail.com>
2024-12-13 09:43:32 +0900

    refactoring, comment delete

--- src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.ll.jpa.domain.post.post.entity;
 
+import com.ll.jpa.domain.post.postComment.entity.PostComment;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -7,6 +8,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -40,4 +43,18 @@ public class Post {
     private String content;
 
     private boolean blind;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<PostComment> comments = new ArrayList<>();
+
+    public void addComment(String content) {
+        PostComment postComment = PostComment
+                .builder()
+                .post(this)
+                .content(content)
+                .build();
+
+        comments.add(postComment);
+    }
 }
--- src/main/java/com/ll/jpa/global/initData/BaseInitData.java
+++ src/main/java/com/ll/jpa/global/initData/BaseInitData.java
@@ -2,7 +2,6 @@ package com.ll.jpa.global.initData;
 
 import com.ll.jpa.domain.post.post.entity.Post;
 import com.ll.jpa.domain.post.post.service.PostService;
-import com.ll.jpa.domain.post.postComment.entity.PostComment;
 import com.ll.jpa.domain.post.postComment.service.PostCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,19 +37,22 @@ public class BaseInitData {
         Post post2 = postService.write("title2", "content2");
         Post post3 = postService.write("title3", "content3");
 
-        PostComment postComment1 = postCommentService.write(post1, "comment1");
-        PostComment postComment2 = postCommentService.write(post1, "comment2");
-        PostComment postComment3 = postCommentService.write(post2, "comment3");
+        post1.addComment(
+                "comment1"
+        );
+
+        post1.addComment(
+                "comment2"
+        );
+
+        post2.addComment(
+                "comment3"
+        );
     }
 
     @Transactional
     public void work2() {
-        Post post3 = postService.findById(3).get();
-        PostComment newPostComment = postCommentService.write(post3, "new comment");
 
-        PostComment postComment3 = postCommentService.findById(3).get();
-
-        postCommentService.delete(postComment3);
     }
 
     @Transactional

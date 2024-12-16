c2c342820d87dd1fec4f2e45b9cd3056df8d6392
solji <solji.choi89@gmail.com>
2024-12-16 10:49:50 +0900

    ㅅtest post write

---
+++ src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
@@ -0,0 +1,21 @@
+package com.ll.jpa.domain.post.post.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PostServiceTest {
+    @Autowired
+    private PostService postService;
+
+    @Test
+    @DisplayName("글 2개 생성")
+    public void t1() {
+        postService.write("제목1", "내용1");
+        postService.write("제목2", "내용2");
+    }
+}

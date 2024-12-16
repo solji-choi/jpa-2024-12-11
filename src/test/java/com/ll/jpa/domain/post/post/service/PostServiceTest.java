4fd2a3aeb79ae6ab7ad9529ed34babe9f22d2a3e
solji <solji.choi89@gmail.com>
2024-12-16 10:55:20 +0900

    test rollback

--- src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/ll/jpa/domain/post/post/service/PostServiceTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -14,6 +16,8 @@ public class PostServiceTest {
 
     @Test
     @DisplayName("글 2개 생성")
+    @Transactional
+    @Rollback
     public void t1() {
         postService.write("제목1", "내용1");
         postService.write("제목2", "내용2");
